a8185153910decad62404e97d231815af84d20ce
nepcore <nepcore@griem.xyz>
2017-12-01 17:07:34 +0100

    make the cleanup finds not stop the build

--- stage2/03-cleanup/00-run.sh
+++ stage2/03-cleanup/00-run.sh
@@ -2,5 +2,5 @@
 
 rm -rf ${ROOTFS_DIR}/usr/share/locale/*
 rm -rf ${ROOTFS_DIR}/usr/share/man/*
-find ${ROOTFS_DIR}/usr/share/doc/* \! -name 'copyright' -delete 
-find ${ROOTFS_DIR}/usr/share/doc/* -type d -empty -delete
+find ${ROOTFS_DIR}/usr/share/doc/* \! -name 'copyright' -delete || true
+find ${ROOTFS_DIR}/usr/share/doc/* -type d -empty -delete || true
