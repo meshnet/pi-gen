fdf126f966ccb84c716a19860ab1d03f5b72db55
Reavershark <jonasty1999@gmail.com>
2017-12-01 17:52:49 +0100

    Removed fallback kernel (hardcoded version)

--- stage2/03-cleanup/00-run.sh
+++ stage2/03-cleanup/00-run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
+#docs
 rm -rf ${ROOTFS_DIR}/usr/share/locale/*
 rm -rf ${ROOTFS_DIR}/usr/share/man/*
 find ${ROOTFS_DIR}/usr/share/doc/* \! -name 'copyright' -delete || true
 find ${ROOTFS_DIR}/usr/share/doc/* -type d -empty -delete || true
+
+#unnessecary fallback kernel (hardcoded)
+rm -rf ${ROOTFS_DIR}/boot/kernel.img
+rm -rf ${ROOTFS_DIR}/lib/modules/4.9.35+/
