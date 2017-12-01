f6eecba8dec64a39df22bd10411399d2f498cf8d
nepcore <nepcore@griem.xyz>
2017-12-01 14:53:19 +0100

    clean up doc but keep copyright files

--- stage2/03-cleanup/00-run.sh
+++ stage2/03-cleanup/00-run.sh
@@ -2,3 +2,15 @@
 
 rm -rf ${ROOTFS_DIR}/usr/share/locale/*
 rm -rf ${ROOTFS_DIR}/usr/share/man/*
+
+# Remove contents of the doc dir but keep the copyright files for license reasons
+CLEAN_DOC_DIR=${ROOTFS_DIR}/usr/share/clean_doc
+mkdir -p ${CLEAN_DOC_DIR}
+for DOC_DIR in ${ROOTFS_DIR}/usr/share/doc/*; do
+	if [ -f ${DOC_DIR}/copyright ]; then
+		mkdir ${CLEAN_DOC_DIR}/$(basename ${DOC_DIR})
+		cp ${DOC_DIR}/copyright ${CLEAN_DOC_DIR}/$(basename ${DOC_DIR})/copyright
+	fi
+done
+rm -rf ${ROOTFS_DIR}/usr/share/doc
+mv ${CLEAN_DOC_DIR} ${ROOTFS_DIR}/usr/share/doc
